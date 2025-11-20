aa889246ef7a641dbdea9abe2ee193625800a837
jwchavarrro <jwchavarro@prevalentware.com>
2025-11-20 17:33:10 -0500

    feat(home): enhance Home component with button integration and layout improvements
    - Added button functionality to the Home component, allowing dynamic navigation based on HOME_DATA.
    - Updated layout structure to use a section element for better semantic HTML.
    - Improved spacing and organization of text elements for a cleaner presentation.

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,47 @@
  * @description Página principal de la aplicación.
  */
 
+import Link from "next/link";
+
 // Import of components custom
-import { Title, Text } from "@/components/atomic-design/atoms";
+import { Title, Text, Button } from "@/components/atomic-design/atoms";
+
+// Import of utilities
+import { HOME_DATA } from "@/components/pages";
 
 export default function Home() {
   return (
     <div className="h-[calc(100vh-96px)]">
-      <div className="h-full w-full max-w-4/5 mx-auto grid grid-cols-2 content-center gap-5 animate-in slide-in-from-top-10 duration-300 ease-in-out">
+      <section className="h-full w-full max-w-4/5 mx-auto grid grid-cols-2 content-center gap-5 animate-in slide-in-from-top-10 duration-300 ease-in-out">
         <div className="size-full flex items-center justify-center">
           <div className="w-96 h-96 mx-auto bg-foreground rounded-full" />
         </div>
-        <div className="max-w-2xl">
-          <Text className="uppercase tracking-widest font-bold">Semi-Senior</Text>
-          <Title variant="gradient" >
-            Desarrollador Frontend | Especialista en React & Next.js
-          </Title>
-          <Text>
-            Ayudo a construir aplicaciones web modernas, escalables y centradas
-            en el usuario. Mi experiencia abarca desde prototipos rápidos hasta
-            aplicaciones complejas.
-          </Text>
+        <div className="max-w-2xl space-y-5">
+          <div>
+            <Text className="uppercase tracking-widest font-bold">
+              Semi-Senior
+            </Text>
+            <Title variant="gradient">
+              Desarrollador Frontend | Especialista en React & Next.js
+            </Title>
+            <Text>
+              Ayudo a construir aplicaciones web modernas, escalables y
+              centradas en el usuario. Mi experiencia abarca desde prototipos
+              rápidos hasta aplicaciones complejas.
+            </Text>
+          </div>
+          <div className="flex gap-2">
+            {HOME_DATA?.buttons?.map((button) => (
+              <Button key={button.label} size="lg" className="hover:cursor-pointer hover:bg-transparent" asChild>
+                <Link href={button.url}>
+                <button.icon />
+                {button.label}
+                </Link>
+              </Button>
+            ))}
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
