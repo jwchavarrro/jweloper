3df369d62246b4f004d7b956eaae2d753e5f50ee
jwchavarrro <jwchavarro@prevalentware.com>
2025-11-20 12:13:05 -0500

    feat(ia-chat): enhance main chat page layout and integrate input search component
    - Updated IaChat component to improve layout with a centered design.
    - Integrated TypographyH1 for a more user-friendly heading.
    - Added InputSearch component for user queries, enhancing interactivity.

--- components/atomic-design/atoms/index.ts
+++ components/atomic-design/atoms/index.ts
@@ -6,4 +6,5 @@
 export { Button } from "./button"
 export { Heading } from "./heading"
 export { Text } from "./text"
+export { InputText } from "./inputs/input-text"
 
---
+++ components/atomic-design/atoms/inputs/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @file index.ts
+ * @description Archivo de índice para los componentes de inputs.
+ */
+
+export * from "./input-text";
