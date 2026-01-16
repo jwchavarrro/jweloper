e5810ddbf16ee7e062d04bcb00222a9493f146ec
jweloper <jwchavarro023@gmail.com>
2026-01-16 16:45:34 -0500

    refactor(layout): simplify static parameters function
    - Changed the static parameters function to a non-async function for improved performance.
    - Maintained the return of a fixed set of locales, ensuring consistency in locale handling.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -37,7 +37,7 @@ export async function generateMetadata({
   };
 }
 
-export async function generateStaticParams() {
+export function generateStaticParams() {
   return [{ locale: "es" }, { locale: "en" }];
 }
 
